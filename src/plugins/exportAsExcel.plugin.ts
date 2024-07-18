e5e1944843d266fb1d5e520d1e6fca31e3d51a43
joaquin feola <joacoarielfeola@gmail.com>
2024-07-18 13:49:57 -0300

    s

--- src/plugins/exportAsExcel.plugin.ts
+++ src/plugins/exportAsExcel.plugin.ts
@@ -1,18 +1,28 @@
 
 
 
-import * as excelXlsx from 'xlsx';
+// import * as excelXlsx from 'xlsx';
+// import * as excelXlsx from 'exceljs';
+import * as exceljs from 'exceljs';
+
+// export const excel = {
+//     exportAsExcelWithJsonData: async <T>(jsonData: T[], filename: string) => {
+//         const workbook = excelXlsx.utils.book_new();
+//         const worksheet = excelXlsx.utils.json_to_sheet(jsonData);
+//         excelXlsx.utils.book_append_sheet(workbook, worksheet);
+//         await excelXlsx.writeFile( workbook, `${filename}.xlsx`);
+
+//     }
+// }
 
 export const excel = {
-    exportAsExcelWithJsonData: async <T>(jsonData: T[], filename: string) => {
-        const workbook = excelXlsx.utils.book_new();
-        const worksheet = excelXlsx.utils.json_to_sheet(jsonData);
-        excelXlsx.utils.book_append_sheet(workbook, worksheet);
-        await excelXlsx.writeFile( workbook, `${filename}.xlsx`);
+    exportAsExcelWithJsonData: async <T>(jsonData: T[], filename: string) => { },
+    exportAsExcel: async<T>(json: T[], filename: string) => {
+        const workbook = new exceljs.Workbook();
+        workbook.created = new Date();
+        const sheet = workbook.addWorksheet('sheet1', { properties: { tabColor: { argb: '#304' } } })
+        
 
     }
 }
 
-
-    
-    
